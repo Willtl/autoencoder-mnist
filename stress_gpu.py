1fa8baeb2aaf34dd3ae66af9ed9d367973db1043
Willian Tessaro Lunardi <williantessarolunardi@gmail.com>
2020-11-14 22:53:40 +0100

    Fix gpu stuff

--- stress_gpu.py
+++ stress_gpu.py
@@ -15,14 +15,15 @@ loader = DataLoader(
     pin_memory=True
 )
 
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 model = models.resnet50()
-model.to('cuda:0')
-criterion = nn.CrossEntropyLoss()
+model.to(device)
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+criterion = nn.CrossEntropyLoss().to(device)
 
 for data, target in loader:
-    data = data.to('cuda', non_blocking=True)
-    target = target.to('cuda', non_blocking=True)
+    data = data.to(device, non_blocking=True)
+    target = target.to(device, non_blocking=True)
     optimizer.zero_grad()
     output = model(data)
     loss = criterion(output, target)
